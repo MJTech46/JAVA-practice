21fb3d2d1fbe670f02632d642cfeafb3a13a26b5
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-09-22 23:01:09 +0530

    clickCounter updated && notepad micro project is started

--- Swing GUI/SwingClickCounter.java
+++ Swing GUI/SwingClickCounter.java
@@ -10,20 +10,27 @@ public class SwingClickCounter {
         JFrame frame = new JFrame("Click Counter");
 
         // Create a JLabel to display the counter value
-        JLabel label = new JLabel("Clicks: 0", JLabel.CENTER);
+        JLabel label = new JLabel("Click the button :)", JLabel.CENTER);
 
         // Create a JButton
         JButton button = new JButton("Click Me");
 
-        // Create a counter variable
-        final int[] count = {0};
+        // Create counter variables
+        final int[] count = {1};
+        final int maxCount = 10;
 
         // Add an ActionListener to the button to handle button clicks
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Increment the counter and update the label text
-                count[0]++;
-                label.setText("Clicks: " + count[0]);
+                if(count[0] < maxCount){
+                    // Update the label text and increment the counter
+                    label.setText("Clicks: " + count[0]);
+                    count[0]++;
+                } else {
+                    label.setText("Congratulation! you clicked "+ count[0] +" times");
+                    frame.remove(button);
+                }
+                
             }
         });
 
